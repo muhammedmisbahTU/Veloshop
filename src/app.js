7b9b75e1a9dc2f32cf16cd6da10fb148552ab0fd
Misbah <misbahtu2@gmail.com>
2026-06-02 13:06:57 +0530

    feat:setup express layout configuration , mvc ,

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import sessionConfig from "./src/config/session.js";
+import sessionConfig from "./config/session.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import expressLayouts from 'express-ejs-layouts';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
-
 dotenv.config();
-
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(sessionConfig);
 
-//  Test Route
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ status: 'healthy', message: 'Veloshop API is running' });
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', authRoutes);
+
+
 
 // 5. Start Server listening immediately
 const PORT = process.env.PORT || 5000;
